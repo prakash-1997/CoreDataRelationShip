6d033193c1a3e0500ffcc6bc083b82d666d97adc
prakash <rathodprakash1997@outlook.com>
2020-08-30 18:15:08 +0530

    first

--- CoreDataRelationShip/ViewController.swift
+++ CoreDataRelationShip/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
